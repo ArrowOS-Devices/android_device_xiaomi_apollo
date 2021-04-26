e978b73b9b683eec32fba405f5fe29333fd738fb
Ramii Ahmed <ramy@ahmedramy.com>
2021-04-26 22:14:28 +0000

    apollon: adapt to Fluid

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_apollon.mk
+    $(LOCAL_DIR)/fluid_apollon.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_apollon-user \
-    lineage_apollon-userdebug \
-    lineage_apollon-eng
+    fluid_apollon-user \
+    fluid_apollon-userdebug \
+    fluid_apollon-eng
--- fluid_apollon.mk
+++ fluid_apollon.mk
@@ -8,21 +8,22 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
-
 # Inherit from apollon device
 $(call inherit-product, device/xiaomi/apollon/device.mk)
 
-PRODUCT_NAME := lineage_apollon
+# Inherit some common Fluid stuff.
+$(call inherit-product, vendor/fluid/config/common_full_phone.mk)
+
+PRODUCT_NAME := fluid_apollon
 PRODUCT_DEVICE := apollon
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Xiaomi Mi 10T
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
-
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="apollo-user 11 RKQ1.200826.002 V12.1.3.0.RJDMIXM release-keys"
-
-BUILD_FINGERPRINT := Xiaomi/apollo_global/apollo:11/RKQ1.200826.002/V12.1.3.0.RJDMIXM:user/release-keys
+IS_PHONE := true
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
+FLUID_BUILD_TYPE := OFFICIAL
+PRODUCT_PRODUCT_PROPERTIES += \
+  ro.fluid.maintainer=Ramisky \
+  ro.fluid.cpu=SDM865
