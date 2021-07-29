1284246249b09f38243a9e1cc9397072ff325791
Dobsgw <george.w.dobson@gmail.com>
2021-07-29 06:33:30 +0100

    apollo: rebrand from apollon to apollo
    For consistency with updates to ArrowOS

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -4,7 +4,7 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-DEVICE_PATH := device/xiaomi/apollon
+DEVICE_PATH := device/xiaomi/apollo
 
 # Inherit from sm8250-common
 include device/xiaomi/sm8250-common/BoardConfigCommon.mk
@@ -15,17 +15,17 @@ BUILD_BROKEN_DUP_RULES := true
 TARGET_SCREEN_DENSITY := 440
 
 # Init
-TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):init_xiaomi_apollon
-TARGET_RECOVERY_DEVICE_MODULES := init_xiaomi_apollon
+TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):init_xiaomi_apollo
+TARGET_RECOVERY_DEVICE_MODULES := init_xiaomi_apollo
 
 # Kernel
 TARGET_KERNEL_CONFIG := vendor/apollo_defconfig
 
 # OTA assert
-TARGET_OTA_ASSERT_DEVICE := apollon,apollo,apollopro
+TARGET_OTA_ASSERT_DEVICE := apollo,apollon,apollopro
 
 # Properties
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
 
 # Inherit from the proprietary version
-include vendor/xiaomi/apollon/BoardConfigVendor.mk
+include vendor/xiaomi/apollo/BoardConfigVendor.mk
--- extract-files.sh
+++ extract-files.sh
@@ -22,7 +22,7 @@ fi
 
 set -e
 
-export DEVICE=apollon
+export DEVICE=apollo
 export DEVICE_COMMON=sm8250-common
 export VENDOR=xiaomi
 
