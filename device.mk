0f58c3546f5af7e7707eccf1e3ed6340e7a0f8bc
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-09-15 12:09:31 +0200

    apollo: Move common tree and shipping product API prop to the top
    Change-Id: I6fe21526ece548b49ab6891d99abb0057a304f02

--- device.mk
+++ device.mk
@@ -4,6 +4,11 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Inherit from sm8250-common
+$(call inherit-product, device/xiaomi/sm8250-common/kona.mk)
+
+PRODUCT_SHIPPING_API_LEVEL := 29
+
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
@@ -24,11 +29,6 @@ TARGET_SCREEN_WIDTH := 1080
 # Camera
 PRODUCT_PACKAGES += \
     CutoutRingServiceApollo
-
-PRODUCT_SHIPPING_API_LEVEL := 29
-
-# Inherit from sm8250-common
-$(call inherit-product, device/xiaomi/sm8250-common/kona.mk)
 
 # Inherit from vendor blobs
 $(call inherit-product, vendor/xiaomi/apollo/apollo-vendor.mk)
