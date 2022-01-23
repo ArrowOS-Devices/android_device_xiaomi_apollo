223c49b32dfa363deeb664826486de740c53235f
Ramii Ahmed <ramy@ahmedramy.com>
2022-01-23 22:45:44 +0200

    apollon: decommonize releasetools extensions

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -27,5 +27,8 @@ TARGET_OTA_ASSERT_DEVICE := apollo,apollon,apollopro
 # Properties
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
 
+# Releasetools
+TARGET_RELEASETOOLS_EXTENSIONS := $(DEVICE_PATH)
+
 # Inherit from the proprietary version
 include vendor/xiaomi/apollo/BoardConfigVendor.mk
