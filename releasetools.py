06c8981684fb207baf838fff946e34fe0e87863b
Ramii Ahmed <ramy@ahmedramy.com>
2022-01-30 03:30:16 +0200

    apollon: releasetools: clean-up firmware images into an array & a loop
    uh, clean?

--- releasetools.py
+++ releasetools.py
@@ -41,24 +41,32 @@ def OTA_InstallEnd(info):
   AddImage(info, "vbmeta.img", "/dev/block/bootdevice/by-name/vbmeta")
   AddImage(info, "vbmeta_system.img", "/dev/block/bootdevice/by-name/vbmeta_system")
 
+  fw_cmd = ""
+
+  fw_map = {
+      'abl.elf': ['abl'],
+      'aop.mbn': ['aop'],
+      'BTFM.bin': ['bluetooth'],
+      'cmnlib.mbn': ['cmnlib'],
+      'cmnlib64.mbn': ['cmnlib64'],
+      'devcfg.mbn': ['devcfg'],
+      'dspso.bin': ['dsp'],
+      'featenabler.mbn': ['featenabler'],
+      'hyp.mbn': ['hyp'],
+      'km4.mbn': ['keymaster'],
+      'NON-HLOS.elf': ['modem'],
+      'qupv3fw.elf': ['qupfw'],
+      'storsec.mbn': ['storsec'],
+      'tz.mbn': ['tz'],
+      'uefi_sec.mbn': ['uefisecapp'],
+      'xbl_4.elf': ['xbl_4'],
+      'xbl_5.elf': ['xbl_5'],
+      'xbl_config_4.elf': ['xbl_config_4'],
+      'xbl_config_5.elf': ['xbl_config_5']
+      }
+
   # Firmware
-  info.script.AppendExtra('package_extract_file("install/firmware-update/abl.elf", "/dev/block/bootdevice/by-name/abl");')
-  info.script.AppendExtra('package_extract_file("install/firmware-update/aop.mbn", "/dev/block/bootdevice/by-name/aop");')
-  info.script.AppendExtra('package_extract_file("install/firmware-update/BTFM.bin", "/dev/block/bootdevice/by-name/bluetooth");')
-  info.script.AppendExtra('package_extract_file("install/firmware-update/cmnlib.mbn", "/dev/block/bootdevice/by-name/cmnlib");')
-  info.script.AppendExtra('package_extract_file("install/firmware-update/cmnlib64.mbn", "/dev/block/bootdevice/by-name/cmnlib64");')
-  info.script.AppendExtra('package_extract_file("install/firmware-update/devcfg.mbn", "/dev/block/bootdevice/by-name/devcfg");')
-  info.script.AppendExtra('package_extract_file("install/firmware-update/dspso.bin", "/dev/block/bootdevice/by-name/dsp");')
-  info.script.AppendExtra('package_extract_file("install/firmware-update/featenabler.mbn", "/dev/block/bootdevice/by-name/featenabler");')
-  info.script.AppendExtra('package_extract_file("install/firmware-update/hyp.mbn", "/dev/block/bootdevice/by-name/hyp");')
-  info.script.AppendExtra('package_extract_file("install/firmware-update/km4.mbn", "/dev/block/bootdevice/by-name/keymaster");')
-  info.script.AppendExtra('package_extract_file("install/firmware-update/NON-HLOS.bin", "/dev/block/bootdevice/by-name/modem");')
-  info.script.AppendExtra('package_extract_file("install/firmware-update/qupv3fw.elf", "/dev/block/bootdevice/by-name/qupfw");')
-  info.script.AppendExtra('package_extract_file("install/firmware-update/storsec.mbn", "/dev/block/bootdevice/by-name/storsec");')
-  info.script.AppendExtra('package_extract_file("install/firmware-update/tz.mbn", "/dev/block/bootdevice/by-name/tz");')
-  info.script.AppendExtra('package_extract_file("install/firmware-update/uefi_sec.mbn", "/dev/block/bootdevice/by-name/uefisecapp");')
-  info.script.AppendExtra('package_extract_file("install/firmware-update/xbl_4.elf", "/dev/block/bootdevice/by-name/xbl_4");')
-  info.script.AppendExtra('package_extract_file("install/firmware-update/xbl_5.elf", "/dev/block/bootdevice/by-name/xbl_5");')
-  info.script.AppendExtra('package_extract_file("install/firmware-update/xbl_config_4.elf", "/dev/block/bootdevice/by-name/xbl_config_4");')
-  info.script.AppendExtra('package_extract_file("install/firmware-update/xbl_config_5.elf", "/dev/block/bootdevice/by-name/xbl_config_5");')
-  return
+  for fw in fw_map.keys():
+      for part in fw_map[fw]:
+          fw_cmd += 'package_extract_file("install/firmware-update/{}", "/dev/block/bootdevice/by-name/{}");\n'.format(fw, part)
+  info.script.AppendExtra(fw_cmd)
